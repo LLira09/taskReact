203210dd53ada87d8035983a0f3f8410baae2232
Jake Lira <llira09@gmail.com>
2021-01-28 13:21:21 -0600

    added ability to delete task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:20pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Dentist Appointment',
+      day: 'Feb 5th at 12:00pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Dinner With Wife',
+      day: 'Feb 5th at 8:20pm',
+      reminder: true
+    },
+    {
+      id: 4,
+      text: 'App Due',
+      day: 'Feb 10th at 10:00am',
+      reminder: false
+    }
+  ])
+
+  // Delete Task
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No tasks'
+      )}
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,9 @@
-import { useState } from 'react'
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:20pm',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Dentist Appointment',
-      day: 'Feb 5th at 12:00pm',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Dinner With Wife',
-      day: 'Feb 5th at 8:20pm',
-      reminder: true
-    },
-    {
-      id: 4,
-      text: 'App Due',
-      day: 'Feb 10th at 10:00am',
-      reminder: false
-    }
-  ])
+import Task from './Task'
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map(task => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   )
